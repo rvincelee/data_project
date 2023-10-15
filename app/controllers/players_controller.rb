da6a1a9ce8f705709b24f65710e3373d2545f704
rvincelee <vincelee_05@yahoo.com>
2023-10-15 13:34:05 -0500

    Created search functionality

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,7 +1,15 @@
 class PlayersController < ApplicationController
   def index
-    @players = Player.order(:first_name, :last_name).paginate(page: params[:page], per_page: 40)
-  end  
+    @players = Player.order(:first_name, :last_name)
+
+    if params[:search].present?
+      @players = @players.where('first_name LIKE ? OR last_name LIKE ?', "%#{params[:search]}%", "%#{params[:search]}%")
+    end
+
+    @players = @players.where(position: params[:position]) if params[:position].present?
+
+    @players = @players.paginate(page: params[:page], per_page: 40)
+  end
 
   def show
     @player = Player.find(params[:id])
--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,10 +1,18 @@
 class TeamsController < ApplicationController
   def index
-    @teams = Team.all
+    @teams = Team.order(:name).all
   end
 
   def show
     @team = Team.find(params[:id])
-    @players = @team.players.order(:first_name, :last_name).paginate(page: params[:page], per_page: 10)
+
+    @players = if params[:search].present?
+                 @team.players.where('first_name LIKE ? OR last_name LIKE ?', "%#{params[:search]}%",
+                                     "%#{params[:search]}%")
+               else
+                 @team.players.order(:first_name, :last_name)
+               end
+
+    @players = @players.paginate(page: params[:page], per_page: 10)
   end
 end
